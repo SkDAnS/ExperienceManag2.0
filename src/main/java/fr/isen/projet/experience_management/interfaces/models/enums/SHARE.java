da7971fe06fd5e4b4ba5f05d86ab67024af49fb1
LaraCrafte <clara.langev@gmail.com>
2025-01-07 09:32:27 +0100

    nouveau modelio
    il n'était plus en model Driven mais en Trip alors que je n'ai pas refais de modifs dessus.

--- src/main/java/fr/isen/projet/experience_management/interfaces/models/enums/SHARE.java
+++ src/main/java/fr/isen/projet/experience_management/interfaces/models/enums/SHARE.java
@@ -1,9 +1,16 @@
 package fr.isen.projet.experience_management.interfaces.models.enums;
 
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("c6670786-0663-4e1c-9036-82c1b577220d")
+//begin of modifiable zone(Javadoc).......C/c6670786-0663-4e1c-9036-82c1b577220d
+
+//end of modifiable zone(Javadoc).........E/c6670786-0663-4e1c-9036-82c1b577220d
 public enum SHARE {
+//begin of modifiable zone(Javadoc).......C/48354d81-5865-477a-bab0-e8e33570cd4f
+
+//end of modifiable zone(Javadoc).........E/48354d81-5865-477a-bab0-e8e33570cd4f
     PRIVATE,
+//begin of modifiable zone(Javadoc).......C/07d91be3-85d5-430b-8e0a-1253033e20d5
+
+//end of modifiable zone(Javadoc).........E/07d91be3-85d5-430b-8e0a-1253033e20d5
     PUBLIC;
 }
